32aca9f5383569ec57f3a1a7092398dabf2e8e03
Omar Abdelrahman <omarahmed8k@gmail.com>
2022-09-20 19:06:59 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/about">
+        <About />
+      </Route>
+    </Router>
   );
 }
 
