27df859d381952a5acd22ef24e96511d606004d0
Omar Abdelrahman <omarahmed8k@gmail.com>
2022-09-21 04:10:44 +0200

    assets

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -2,7 +2,6 @@ import "./style.css";
 
 export default function Services(props) {
   const { mode } = props;
-  console.log(mode);
   return (
     <div className="services">
       <h1>{mode === "light" ? "Services Light" : "Services Dark"}</h1>
