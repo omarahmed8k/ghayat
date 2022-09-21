c817b82045efd01a9152d8de9a87c4d52dc4bd54
Omar Abdelrahman <omarahmed8k@gmail.com>
2022-09-21 04:44:09 +0200

    Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
+import { ReactComponent as Menu } from "../../assets/images/menu.svg";
 import "./style.css";
-import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [menu, setMenu] = useState(true);
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">about</Link>
-      <Link to="/services">services</Link>
-      <Link to="/contact">contact</Link>
+    <div className="header">
+      <div className="container">
+        <div className="header-flex">
+          <div className="header-links">
+            {menu && (
+              <button onClick={setMenu(!menu)}>
+                <Menu />
+              </button>
+            )}
+            <Link className="main-btn" to="/contact">
+              تواصل معنا
+            </Link>
+            <NavLink activeClassName="active-link" to="/services">
+              خدماتنا
+            </NavLink>
+            <NavLink activeClassName="active-link" to="/about">
+              من نحن
+            </NavLink>
+            <NavLink to="/">الرئيسية</NavLink>
+          </div>
+          <Link className="header-logo" to="/">
+            <Logo />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
